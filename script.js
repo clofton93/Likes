38d1384ff8d7855523c63b6cb4455436a4da239f
clofton93 <97202396+clofton93@users.noreply.github.com>
2022-01-26 21:06:31 -0700

    final

---
+++ script.js
@@ -0,0 +1,32 @@
+var count1 = 9;
+var countNeil = document.querySelector("#count1");
+
+console.log(countNeil);
+
+function like1(){
+    count1 ++;
+    countNeil.innerHTML = count1;
+    console.log(count1);
+}
+
+var count2 = 12;
+var countNichole = document.querySelector("#count2");
+
+console.log(countNichole);
+
+function like2(){
+    count2 ++;
+    countNichole.innerText = count2;
+    console.log(count2);
+}
+
+var count3 = 9;
+var countJim = document.querySelector("#count3");
+
+console.log(countJim);
+
+function like3(){
+    count3 ++;
+    countJim.innerHTML = count3;
+    console.log(count3);
+}
